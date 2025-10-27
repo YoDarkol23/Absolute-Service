611c59380fe49eda80a409e33560873ff812656b
shifrovannaya <evgeniaarhipova96@gmail.com>
2025-10-27 18:45:02 +0300

    Create client_menu.cpp

---
+++ car_delivery/client/src/client_menu.cpp
@@ -0,0 +1,8 @@
+#include "client_menu.h"
+#include <iostream>
+
+void showMenu() {
+    std::cout << "=== Меню клиента ===" << std::endl;
+    std::cout << "1. Посмотреть список машин" << std::endl;
+    std::cout << "2. Выйти" << std::endl;
+}
