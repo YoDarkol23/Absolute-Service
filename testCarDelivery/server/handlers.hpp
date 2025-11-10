2adc80ce29520c62a6ea4ea18e208a687342eada
NoSalt <ivankirin78@gmail.com>
2025-11-10 20:27:16 +0300

    Add files via upload

--- testCarDelivery/server/handlers.hpp
+++ testCarDelivery/server/handlers.hpp
@@ -17,5 +17,11 @@ std::string handle_get_cities();
 std::string handle_get_documents();
 std::string handle_get_delivery();
 
+// Дополнительные эндпоинты для клиента
+std::string handle_get_cars_specs(const std::string& query_string);
+std::string handle_get_cars_brand(const std::string& query_string);
+std::string handle_get_delivery_cities();
+std::string handle_get_delivery_process();
+
 // Эндпоинт админки
 std::string handle_post_admin_login(const std::string& body);
