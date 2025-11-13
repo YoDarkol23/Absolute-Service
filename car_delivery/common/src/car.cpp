26aa88c75fc122ed83b687688bfe879603742699
YoDarkol23 <zenix.k11@gmail.com>
2025-11-13 14:41:26 +0300

    Delete car_delivery/common/src directory

--- car_delivery/common/src/car.cpp
+++
@@ -1,8 +0,0 @@
-#include "car.h"
-
-Car::Car(const std::string& model, int year)
-    : model_(model), year_(year) {}
-
-std::string Car::getInfo() const {
-    return model_ + " (" + std::to_string(year_) + ")";
-}
